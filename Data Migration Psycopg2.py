b9364cf35ae0f27f662a465ba4804a8798261a01
Sundar487 <114070761+Sundar487@users.noreply.github.com>
2025-08-04 17:04:18 +0530

    Update Data Migration Psycopg2.py

--- Data Migration Psycopg2.py
+++ Data Migration Psycopg2.py
@@ -4,33 +4,42 @@ import urllib.request
 
 # Connect to the database
 def lambda_handler(event, context):
-    db = psycopg2.connect(host='database-1.cl80waky6nkw.ap-south-1.rds.amazonaws.com',database='issdata',user='postgres',password='********')
+    db = psycopg2.connect(
+        host='database-1.cl80waky6nkw.ap-south-1.rds.amazonaws.com',
+        database='issdata',
+        user='postgres',
+        password='********'
+    )
 
-    # Define the SQL query to create the table if it doesn't exist already
-    create_table_query = """CREATE TABLE IF NOT EXISTS iss_position (id SERIAL PRIMARY KEY,latitude INTEGER,longitude INTEGER,timestamp INTEGER,message VARCHAR(255))"""
-
-    # Execute the query to create the table
+    create_table_query = """
+        CREATE TABLE IF NOT EXISTS iss_position (
+            id SERIAL PRIMARY KEY,
+            latitude INTEGER,
+            longitude INTEGER,
+            timestamp INTEGER,
+            message VARCHAR(255)
+        )
+    """
+    
     cursor = db.cursor()
     cursor.execute(create_table_query)
     db.commit()
 
+    # Fetch ISS position data from API
     api_url = 'http://api.open-notify.org/iss-now.json'
-
-    # Fetch the data from the API
     with urllib.request.urlopen(api_url) as response:
         data = json.loads(response.read().decode())
 
-    # Extract the latitude and longitude values
+    # Extract values
     latitude = data['iss_position']['latitude']
     longitude = data['iss_position']['longitude']
     timestamp = data['timestamp']
     message = data['message']
 
-    # Define the SQL query to insert the data into the table
-    insert_query = "INSERT INTO iss_position(latitude, longitude, timestamp, message ) VALUES (%s, %s, %s, %s)"
-
-    # Execute the query to insert the data into the table
-    cursor = db.cursor()
+    insert_query = """
+        INSERT INTO iss_position (latitude, longitude, timestamp, message)
+        VALUES (%s, %s, %s, %s)
+    """
     cursor.execute(insert_query, (latitude, longitude, timestamp, message))
     db.commit()
 
@@ -40,4 +49,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
